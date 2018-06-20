82d1ba376443c6177983c553ae62c426a81d2049
Ketan Kale <kaleketan07@gmail.com>
2018-06-20 17:32:43 -0400

    values test

---
+++ src/values/valuestest.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main{
+
+  Printf("Hey" + "There!")
+  Printf(2 + 1)
+  Printf(2/4)
+  Printf(2.00/3)
+  Printf(2/3.00)
+  Printf(true && false)
+  Printf(true || false)
+
+}
