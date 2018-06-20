4201228b5954546bb80e5b00d4a7c0a02b52d75c
Ketan Kale <kaleketan07@gmail.com>
2018-06-20 17:38:09 -0400

    valuestest edited

--- src/values/valuestest.go
+++ src/values/valuestest.go
@@ -2,14 +2,14 @@ package main
 
 import "fmt"
 
-func main{
+func main(){
 
-  Printf("Hey" + "There!")
-  Printf(2 + 1)
-  Printf(2/4)
-  Printf(2.00/3)
-  Printf(2/3.00)
-  Printf(true && false)
-  Printf(true || false)
+  fmt.Printf("Hey" + "There!")
+  fmt.Printf("", 2 + 1)
+  fmt.Printf("", 2/4)
+  fmt.Printf("", 2.00/3)
+  fmt.Printf("", 2/3.00)
+  fmt.Printf("", true && false)
+  fmt.Printf("", true || false)
 
 }
