75fc483ca51c90569f467bcb1f35e26c8522fc06
Ketan Kale <kaleketan07@gmail.com>
2018-06-21 10:47:14 -0400

    variables test

--- src/basics/values/valuestest.go
+++ src/basics/values/valuestest.go
@@ -2,14 +2,15 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 
-  fmt.Printf("Hey" + "There!")
-  fmt.Printf("", 2 + 1)
-  fmt.Printf("", 2/4)
-  fmt.Printf("", 2.00/3)
-  fmt.Printf("", 2/3.00)
-  fmt.Printf("", true && false)
-  fmt.Printf("", true || false)
+  fmt.Println("Hey" + "There!")
+  fmt.Println("2 + 1 = ", 2 + 1)
+  fmt.Println("2/4 = ", 2/4)
+  fmt.Println("5/3 = ", 5/3)
+  fmt.Println("2.00/4 = ", 2.00/4)
+  fmt.Println("2/3.00", 2/3.00)
+  fmt.Println(true && false)
+  fmt.Println(true || false)
 
 }
---
+++ src/basics/variables/variablestest.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+  var a = "initial"
+  fmt.Println(a)
+
+  var b,c,d int = 1,2,3
+  fmt.Println(b,c,d)
+
+  var e = true && false
+
+  fmt.Println(e)
+
+
+  f := "short hand for declaring and initializing a variable"
+  fmt.Println(f)
+
+
+
+}
